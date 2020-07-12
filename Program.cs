d41f80e0a98f92e62feef81d0438b20e9acd09dc
AdiletAldazharbekov <aldajarbekov@gmail.com>
2020-07-12 16:03:51 +0600

    Добавил проверку на пробел и запятые

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.PortableExecutable;
-using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleApp1
 {
@@ -11,80 +9,95 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            string stop;
+            string exit;
             do
             {
-                //Приветствие
                 Greeting();
-                // Ввод данных
                 Input();
+                exit = Ending();
+            } while (exit.ToUpper() != "N");
 
-
-                Console.WriteLine("\n\nДля выхода нажмите на клавишу \"N\"" +
-                    "\nДля продолжения нажмите на любую клавишу");
-                stop = Console.ReadLine();
-                Clear();
-            } while (stop.ToUpper() != "N");
-
-           
         }
-
         public static void Greeting()
         {
             Console.WriteLine("Здравствуйте, вас приветствует программа \"Градусник\"!"+
             "\nДля старта нажмите на любую клавишу.");
+            Console.ReadKey();
             Clear();
         }
         public static void Input()
         {
             double temperature;
-
             List<double> logbook = new List<double>();
 
             int i=1;
             while (true)
             {
-                Console.Write($"\nВведите вашу температуру \n{i} день: ");
+                Console.WriteLine($"Введите вашу температуру \n{i} день:");
                 var input = Console.ReadLine();
                 if (input=="")
                 {
-                    Console.WriteLine("Конец ввода, Нажмите на любую клавишу");
-                    break;
+                    Console.WriteLine("Для просмотра статистики нажмите на \"Enter\"" +
+                        "\nДля того чтобы продолжить нажмите на любую клавишу");
+                    if (Console.ReadLine() == "") break;
                 }
-                double.TryParse(input, out temperature);
-                if (temperature == 0 || temperature <30 || temperature>41)
-                    Console.WriteLine("Ошибка ввода,  данные должны быть в интервале от 30 до 41 градусов");
                 else
                 {
-                    logbook.Add(temperature);//запись в список
-                    i++;
-                }
+                    var input1 = input.Replace(" ", "");
+                    var input2 = input1.Replace(",", ".");
+
+                    double.TryParse(input2, out temperature);
+                    if (temperature == 0 || temperature < 30 || temperature > 41)
+                        Console.WriteLine("Ошибка ввода, данные должны быть в интервале от 30 до 41 градусов");
+                    else
+                    {
+                        logbook.Add(temperature);
+                        i++;
+                    }
+                }               
             }
-            Clear();
-            if (logbook.Count > 0)
-            {
-                Output(logbook);
-            }
-           
+
+            if (logbook.Count > 0) Output(logbook);
                 
         }
         public static void Clear()
         {
-            Console.ReadKey();
             Console.Clear();
         }
+        
         public static void Output (List<double> blok)
         {
+            Clear();
             Console.WriteLine($"Самая низкая температура: {blok.Min(): 0.0}");
             Console.WriteLine($"Самая высокая температура: {blok.Max(): 0.0}");
             Console.WriteLine($"Средняя температура: {blok.Average(): 0.0}");
-            Console.WriteLine($"Количество дней с минимальной температурой: {blok.Where(t => t == blok.Min()).Count()}");
+
+            Console.WriteLine($"\nКоличество дней с минимальной температурой: {blok.Where(t => t == blok.Min()).Count()}");
             Console.WriteLine($"Количество дней с максимальной температурой: {blok.Where(t => t == blok.Max()).Count()}");
-            Console.WriteLine($"Количество дней с нормальной температурой: {blok.Where(t => t <= 37.5).Count()}");
+            
+            // Самые сильные скочки (рост и спад)  температуры и в какой день это было 
+
+            Console.WriteLine($"\nКоличество дней с нормальной температурой: {blok.Where(t => t <= 37.5).Count()}");
             Console.WriteLine($"Количество дней с высокой температурой: {blok.Where(t => t <= 38.3).Count()- blok.Where(t => t <= 37.5).Count()}");
             Console.WriteLine($"Количество дней с очень высокой температурой: {blok.Where(t => t > 38.3).Count()}");
-            // Самые сильные скочки температуры
+        }
 
+        public static void Osut()
+        {
+            string str = Console.ReadLine();
+            var str1 = str.Replace(" ", "");
+            var str2 = str1.Replace(",", ".");
+
+           
+            Console.WriteLine(str2);
+            
+        }
+
+        private static string Ending()
+        {
+            Console.WriteLine("\n\nДля выхода нажмите на клавишу \"N\"" +
+                    "\nНачать заново нажмите на любую клавишу");
+            return Console.ReadLine();
         }
     }
 }
